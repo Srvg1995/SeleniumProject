c337bb6a04508c4113cd9536eb01316274eb277b
GAGAN M S <gaganms15@gmail.com>
2024-05-18 10:57:53 +0530

    123

--- SeleniumProjectTOLearnMaven/src/test/java/pac1/ContactTest.java
+++ SeleniumProjectTOLearnMaven/src/test/java/pac1/ContactTest.java
@@ -7,7 +7,6 @@ public class ContactTest {
 
 	@Test
 	public void createContactTest() {
-		System.out.println("execute createContactTest");
 		String BROWSER = System.getProperty("browser","chrome"); //Here "chrome" is mentioned,bcz if you forgot to pass the command through CMD line also ,it will take "chrome" as a browser.Similarly,we can pass other credentials also(like,URL,UN,PW)inside this program only by putting comma after the credentials. 
 		String URL = System.getProperty("url");
 		String UN = System.getProperty("username");
@@ -17,6 +16,8 @@ public class ContactTest {
 		System.out.println(URL);
 		System.out.println(UN);
 		System.out.println(PW);
+		System.out.println("execute createContactTest");
+
 		
 	}
 
